14a213ee3bc918f53ffd60576a6ddda6d321ff4c
Casey Manning <kcgrind@gmail.com>
2015-09-19 11:11:24 -0700

    worked on player movement

--- src/player/PlayerMovement.java
+++ src/player/PlayerMovement.java
@@ -9,35 +9,41 @@ public class PlayerMovement implements KeyListener {
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		
+
 		if(e.getKeyCode() == KeyEvent.VK_W) {
-			if(Engine.map[Engine.characterLocation.x][Engine.characterLocation.y - 1].isPassable()) {
-				
+			if(Engine.map[Engine.characterLocation.x][Engine.characterLocation.y - 1].IsPassable()) {
+
 			}
-			
+
 		} else if(e.getKeyCode() == KeyEvent.VK_A) {
-			
-			
+			if(Engine.map[Engine.characterLocation.x - 1][Engine.characterLocation.y].IsPassable()) {
+
+			}
+
 		} else if(e.getKeyCode() == KeyEvent.VK_S) {
-			
-			
+			if(Engine.map[Engine.characterLocation.x][Engine.characterLocation.y + 1].IsPassable()) {
+
+			}
+
 		} else if(e.getKeyCode() == KeyEvent.VK_D) {
-			
-			
+			if(Engine.map[Engine.characterLocation.x + 1][Engine.characterLocation.y].IsPassable()) {
+
+			}
+
 		}
-		
+
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		
-		
+
+
 	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
-		
-		
+
+
 	}
 
 }
