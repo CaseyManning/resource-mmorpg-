a4a1486ce8123b6cbc9a3468600da2ddac7eb5a7
tripplyons <tripplyons@gmail.com>
2015-10-14 19:49:07 -0700

    .

--- src/engine/GamePanel.java
+++ src/engine/GamePanel.java
@@ -6,9 +6,7 @@
 package engine;
 
 import java.awt.BasicStroke;
-import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
