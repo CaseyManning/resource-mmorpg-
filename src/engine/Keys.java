b40e5aac653510bc7ec0a5613acd91af48dc24ce
Casey Manning <kcgrind@gmail.com>
2015-10-16 17:33:40 -0700

    conflicts

--- src/engine/Keys.java
+++ src/engine/Keys.java
@@ -5,20 +5,15 @@ import java.util.Scanner;
 
 public class Keys {
 
-<<<<<<< HEAD
+
 	public static final int UP    = 0;
 	public static final int LEFT  = 1;
 	public static final int DOWN  = 2;
 	public static final int RIGHT = 3;
 	public static boolean sword = false;
-=======
-	public static final int UP     = 0;
-	public static final int LEFT   = 1;
-	public static final int DOWN   = 2;
-	public static final int RIGHT  = 3;
 
 	public static final int PICKUP = 4;
->>>>>>> origin/master
+
 	
 	public static final int NUM_KEYS = 5;
 	
@@ -30,7 +25,7 @@ public class Keys {
 	public static void keySet(int key, boolean b) {
 		System.out.println("keySet(" + Integer.toString(key) + ", " + ((b)?"true":"false") + ")");
 		switch(key) {
-<<<<<<< HEAD
+
 		case KeyEvent.VK_W: keyState[UP]    = b; break;
 		case KeyEvent.VK_A: keyState[LEFT]  = b; break;
 		case KeyEvent.VK_S: keyState[DOWN]  = b; break;
@@ -38,13 +33,7 @@ public class Keys {
 		case KeyEvent.VK_Q: {
 			w.startWizard();
 		}
-=======
-		case KeyEvent.VK_W: keyState[UP]     = b; break;
-		case KeyEvent.VK_A: keyState[LEFT]   = b; break;
-		case KeyEvent.VK_S: keyState[DOWN]   = b; break;
-		case KeyEvent.VK_D: keyState[RIGHT]  = b; break;
-		case KeyEvent.VK_Q: keyState[PICKUP] = b; break;
->>>>>>> origin/master
+
 		default: break;
 		}
 	}
--- src/engine/Wizard.java
+++ src/engine/Wizard.java
@@ -2,13 +2,14 @@ package engine;
 
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 import player.PlayerAttributes;
 
 public class Wizard {
 
-	ArrayList<String> responses = new ArrayList<String>();
+	HashMap<String, Integer> responds = new HashMap<String, Integer>();
 	
 	public Wizard() {
 		
@@ -18,10 +19,7 @@ public class Wizard {
 		System.out.println("I am the Wizard! What do you want me to do?");
 		Scanner scan = new Scanner(System.in);
 		String str = scan.nextLine();
-		if(str.equalsIgnoreCase("gimme a sword")) {
-			System.out.println("I am the Wizard! Take this sword!");
-			//sword = true;
-		}
+		
 		return null;
 	}
 }
